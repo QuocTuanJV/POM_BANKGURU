01b3d2a50138ceab1a63a67ab35bf6625d83f62b
QuocTuanJV <quoctuan.naf@gmail.com>
2019-07-22 21:48:34 +0700

    o nha

--- actions/commons/ExtentReportListener.java
+++ actions/commons/ExtentReportListener.java
@@ -1,8 +1,8 @@
 package commons;
 
 import java.io.File;
-import java.sql.Date;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -21,52 +21,56 @@ import com.relevantcodes.extentreports.LogStatus;
 
 public class ExtentReportListener implements IReporter {
 	private ExtentReports extent;
-	private ExtentTest test;
-	@Override
-	public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
-		extent = new ExtentReports(outputDirectory + File.separator + "BankGuruReport.html", true);
-		for(ISuite suite : suites) {
+
+	public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites,
+			String outputDirectory) {
+		extent = new ExtentReports(outputDirectory + File.separator
+				+ "ExtentReportBankGuru.html", true);
+
+		for (ISuite suite : suites) {
 			Map<String, ISuiteResult> result = suite.getResults();
-			for(ISuiteResult r : result.values()) {
+
+			for (ISuiteResult r : result.values()) {
 				ITestContext context = r.getTestContext();
+
 				buildTestNodes(context.getPassedTests(), LogStatus.PASS);
-				buildTestNodes(context.getPassedTests(), LogStatus.FAIL);
-				buildTestNodes(context.getPassedTests(), LogStatus.SKIP);
+				buildTestNodes(context.getFailedTests(), LogStatus.FAIL);
+				buildTestNodes(context.getSkippedTests(), LogStatus.SKIP);
 			}
 		}
+
 		extent.flush();
 		extent.close();
 	}
-	
+
 	private void buildTestNodes(IResultMap tests, LogStatus status) {
-		if(tests.size() > 0) {
-			for(ITestResult result : tests.getAllResults()) {
-				 test = extent.startTest(result.getMethod().getMethodName());
-				 test.setStartedTime(getTime(result.getStartMillis()));
-				 test.setEndedTime(getTime(result.getEndMillis()));
-				 
-				 for(String group : result.getMethod().getGroups())
-					 test.assignCategory(group);
-				 
-				 if(result.getThrowable() != null) {
-					 test.log(status, result.getThrowable());
-				 }else {
-					 test.log(status, "Test " + status.toString().toLowerCase() + "ed");
-				 }
-				 extent.endTest(test);
+		ExtentTest test;
+
+		if (tests.size() > 0) {
+			for (ITestResult result : tests.getAllResults()) {
+				test = extent.startTest(result.getMethod().getMethodName());
+
+				test.setStartedTime(getTime(result.getStartMillis()));
+				test.setEndedTime(getTime(result.getEndMillis()));
+
+				for (String group : result.getMethod().getGroups())
+					test.assignCategory(group);
+
+				if (result.getThrowable() != null) {
+					test.log(status, result.getThrowable());
+				} else {
+					test.log(status, "Test " + status.toString().toLowerCase()
+							+ "ed");
+				}
+
+				extent.endTest(test);
 			}
 		}
 	}
-	
+
 	private Date getTime(long millis) {
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTimeInMillis(millis);
-		return (Date) calendar.getTime();
+		return calendar.getTime();
 	}
-	
-	public void extendLog(String message) {
-		test.log(LogStatus.INFO, message);
-		Reporter.log(message);
-	}
-
 }
